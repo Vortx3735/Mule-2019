908dfc819ae90586391342b95982fe8d87870829
David Bauducco <antirobo1@gmail.com>
2019-01-07 15:30:16 -0600

    Added Robot2019 files
    Added Robot2019 files as a base

---
+++ src/main/java/org/usfirst/frc/team3735/robot/Autonomous.java
@@ -0,0 +1,17 @@
+package org.usfirst.frc.team3735.robot;
+
+public class Autonomous {
+
+	public Autonomous() {
+
+	}
+
+	public void startCommand() {
+	
+	}
+
+	public void cancel() {
+	
+	}
+
+}
---
+++ src/main/java/org/usfirst/frc/team3735/robot/Robot.java
@@ -0,0 +1,86 @@
+package org.usfirst.frc.team3735.robot;
+
+import org.usfirst.frc.team3735.robot.util.bases.VortxIterative;
+
+/**
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ */
+public class Robot extends VortxIterative {
+	
+	@Override
+	public void robotInit() {
+	
+	}
+	
+	@Override
+	public void robotPeriodic() {
+	
+	}
+
+	@Override
+	public void robotContinuous() {
+	
+	}
+
+	@Override
+	public void autonomousInit() {	
+		
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+	
+	}
+
+	@Override
+	public void autonomousContinuous() {
+		
+	}
+
+	@Override
+    public void teleopInit() {
+
+	}
+	
+	@Override
+	public void teleopPeriodic() {
+   
+	}
+
+	@Override
+	public void teleopContinuous() {
+
+	}
+
+	@Override
+	public void testPeriodic() {
+	}
+
+	@Override
+	public void disabledInit() {
+	
+	}
+
+	@Override
+	public void disabledPeriodic() {
+
+	}
+
+	@Override
+	public void disabledContinuous() {
+		
+	}
+
+	public void log(){
+
+	}
+	
+	public void debugLog(){
+
+	}
+
+}

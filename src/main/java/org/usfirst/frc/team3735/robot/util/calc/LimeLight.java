c30b15dab99a3ce9cfe2ba407ee14cc3c03c618f
Jeremy Hinton <jhin1360@students.kleinisd.net>
2019-02-02 14:29:39 -0600

    optimized code with a constructor for limelight

--- src/main/java/org/usfirst/frc/team3735/robot/util/calc/LimeLight.java
+++ src/main/java/org/usfirst/frc/team3735/robot/util/calc/LimeLight.java
@@ -8,94 +8,72 @@
 package org.usfirst.frc.team3735.robot.util.calc;
 
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 /**
  * Add your docs here.
  */
 public class LimeLight {
 
-    public void log()
-    {
-        System.out.println(getValidTarget());
-        System.out.println(getHorOffset());
-        System.out.println(getVerOffset());
-        System.out.println(getTargetArea());
-        System.out.println(getSkewRotation());
-        System.out.println(getLatency());
-        System.out.println(getSideShortest());
-        System.out.println(getSideLongest());
-        System.out.println(getHorSidelength());
-        System.out.println(getVerSidelength());
-        System.out.println(getPipelineIndex());
-    }
+    NetworkTable table;
+    NetworkTableEntry tx; 
+    NetworkTableEntry ty; 
+    NetworkTableEntry ta; 
+    NetworkTableEntry ts; 
+    NetworkTableEntry tl; 
+    NetworkTableEntry getpipe;
+    
+    //setter dec
+    NetworkTableEntry ledMode;
+    NetworkTableEntry camMode;
+    NetworkTableEntry pipeline;
+    NetworkTableEntry stream;
+    NetworkTableEntry snapshot;
 
-    // getters
 
-    public double getValidTarget() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
-    }
-
-    public double getHorOffset() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tx").getDouble(0);
-    }
-
-    public double getVerOffset() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ty").getDouble(0);
-    }
-
-    public double getTargetArea() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
-    }
-
-    public double getSkewRotation() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("ts").getDouble(0);
-    }
-
-    public double getLatency() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tl").getDouble(0);
-    }
-
-    public double getSideShortest() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tshort").getDouble(0);
-    }
-
-    public double getSideLongest() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tlong").getDouble(0);
-    }
-
-    public double getHorSidelength() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("thor").getDouble(0);
-    }
-
-    public double getVerSidelength() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("tvert").getDouble(0);
-    }
-
-    public double getPipelineIndex() {
-        return NetworkTableInstance.getDefault().getTable("limelight").getEntry("getpipe").getDouble(0);
+    public LimeLight() {
+        table = NetworkTableInstance.getDefault().getTable("limelight");  
+        tx = table.getEntry("tx"); 
+        ty = table.getEntry("ty");
+        ta = table.getEntry("ta");
+        ts = table.getEntry("ts");
+        tl = table.getEntry("tl");
+        getpipe = table.getEntry("getpipe");
+        ledMode = table.getEntry("ledMode");
+        camMode = table.getEntry("camMode");
+        pipeline = table.getEntry("pipeline");
+        stream = table.getEntry("stream");
+        snapshot = table.getEntry("snapshot");
     }
 
     // setters
     
 
-    public void setLedMode(double ledMode) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("ledMode").setNumber(ledMode);
+    public void setLedMode(double ledType) {
+        ledMode.setNumber(ledType);
     }
 
-    public void setCamMode(double CamMode) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("camMode").setNumber(CamMode);
+    public void setCamMode(double CamType) {
+       camMode.setNumber(CamType);
     }
 
     public void setPipeline(double Pipeline) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("pipeline").setNumber(Pipeline);
+        pipeline.setNumber(Pipeline);
     }
 
     public void setStreamMode(double Stream) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("stream").setNumber(Stream);
+        stream.setNumber(Stream);
     }
 
     public void setSnapshot(double Snapshot) {
-        NetworkTableInstance.getDefault().getTable("limelight").getEntry("snapshot").setNumber(Snapshot);
+        snapshot.setNumber(Snapshot);
+    }
+
+    public void log() {
+       System.out.println("tx: " + tx + " ty: " + ty + " ta: " + ta + " ts: " + ts + " tl: " + tl ); 
     }
 
 }
